c9cc52f80b39949d54f794571921d301ccdb633e
Raihan Adam <hallwack.id@gmail.com>
2026-08-16 18:25:06 +0700

    feat: add payment relationship to Order model

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -25,4 +25,9 @@ class Order extends Model
     {
         return $this->hasMany(OrderDetail::class);
     }
+
+    public function payment()
+    {
+        return $this->hasOne(Payment::class);
+    }
 }
