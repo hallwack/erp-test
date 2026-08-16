f77093a070112b0e6bdcf6b33f386dd801324bf2
Raihan Adam <hallwack.id@gmail.com>
2026-08-16 07:31:58 +0700

    feat: update routes to use controllers for home, cart, and checkout functionalities

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,17 @@
 <?php
 
+use App\Http\Controllers\CartController;
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\CheckoutController;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
-Route::inertia('/', 'welcome')->name('home');
+Route::get('/', [HomeController::class, 'home'])->name('home');
+Route::post('/cart/add/{product}', [CartController::class, 'add'])->name('cart.add');
+Route::delete('/cart/remove/{product}', [CartController::class, 'remove'])->name('cart.remove');
+Route::get('/checkout', [CheckoutController::class, 'index'])->name('checkout.index');
+Route::post('/checkout', [CheckoutController::class, 'store'])->name('checkout.store');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::inertia('dashboard', 'dashboard')->name('dashboard');
@@ -15,6 +22,5 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::post('product/{product}/adjust-stock', [ProductController::class, 'storeStockMovement'])->name('product.store-stock-movement');
     Route::resource('product', ProductController::class);
 });
-
 
 require __DIR__.'/settings.php';
